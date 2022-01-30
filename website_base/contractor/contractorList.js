b9f290bc46fd4706139df10220329eb9faff52db
kylediano20 <kyle.diano@tamu.edu>
2022-01-30 09:56:14 -0600

    put contractors into separate js

---
+++ website_base/contractor/contractorList.js
@@ -0,0 +1,17 @@
+var companies = [
+    {
+        "name": "Bob's Plumbing",
+        "phone-number": "832-777-3419",
+        "website": "Bobdoesplumbing.com"
+    },
+    {
+        "name": "Ralph's Flooring",
+        "phone-number": "979-231-2341",
+        "website": "Ralphwillflooryou.com"
+    },
+    {
+        "name": "Smith HVAC",
+        "phone-number": "777-231-4246",
+        "website": "SmithHvacInc.com"
+    }
+]
